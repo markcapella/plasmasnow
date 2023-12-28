d7d2c77e2fd323ed812a4010317775ff0a03d597
Mark Capella <markcapella@twcny.rr.com>
2023-12-27 19:07:05 -0500

    change copyrights

--- src/ui.c
+++ src/ui.c
@@ -1,22 +1,23 @@
 /* -copyright-
-#-#
+#-# 
 #-# plasmasnow: Let it snow on your desktop
 #-# Copyright (C) 1984,1988,1990,1993-1995,2000-2001 Rick Jansen
 #-# 	      2019,2020,2021,2022,2023 Willem Vermin
-#-#
+#-#          2024 Mark Capella
+#-# 
 #-# This program is free software: you can redistribute it and/or modify
 #-# it under the terms of the GNU General Public License as published by
 #-# the Free Software Foundation, either version 3 of the License, or
 #-# (at your option) any later version.
-#-#
+#-# 
 #-# This program is distributed in the hope that it will be useful,
 #-# but WITHOUT ANY WARRANTY; without even the implied warranty of
 #-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #-# GNU General Public License for more details.
-#-#
+#-# 
 #-# You should have received a copy of the GNU General Public License
 #-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#-#
+#-# 
 */
 
 /* How to implement a new button
